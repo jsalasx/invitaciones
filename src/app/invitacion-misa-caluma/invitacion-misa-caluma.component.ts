1d03e44caf7b4114996ac7c40bf8f35bcfd62531
Alejandro Salas <joal.sala@gmail.com>
2024-10-02 16:30:39 -0500

    fix de hora y fondo

--- src/app/invitacion-misa-caluma/invitacion-misa-caluma.component.ts
+++ src/app/invitacion-misa-caluma/invitacion-misa-caluma.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class InvitacionMisaCalumaComponent {
   nombreFallecido: string = 'Segundo Angel Aluisa Analuisa';
   fechaMisa: string = '5 de octubre de 2024';
-  horaMisa: string = '13:00 PM';
+  horaMisa: string = '13h00';
   nombreIglesia: string = 'María Auxiliadora del cantón Caluma ';
   nombreFamilia: string = 'Familia Aluisa ';
   linkGoogleMaps: string = 'https://maps.app.goo.gl/dXBpxJcQp73zhLu88'
